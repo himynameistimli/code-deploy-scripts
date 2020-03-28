dfb0b04c4014b5f775f3d0cfc6b4537bd4a619b6
Tim Li <himynameistimli@gmail.com>
2020-03-28 12:51:24 +0800

    Set installs to happen automatically

--- code_deploy_agent.sh
+++ code_deploy_agent.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-
-echo "Updating Yum..."
+echo " "
+echo "--------------------------------"
+echo "Updating Dependencies..."
+echo "--------------------------------"
 
 sudo yum update -y
-
+sudo yum install ruby -y
+sudo yum install wget -y
+echo " "
+echo "--------------------------------"
 echo "Updating Installing Code Deploy Agent (ap-southeast-1)..."
-sudo yum install ruby
-sudo yum install wget
+echo "--------------------------------"
 
 cd /home/ec2-user
 wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 
+echo " "
+echo "--------------------------------"
 echo "Checking Codedeploy Agent Status"
+echo "--------------------------------"
+
 sudo service codedeploy-agent status
 
